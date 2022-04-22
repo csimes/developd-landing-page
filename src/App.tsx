84c17e9fcaf2855103563cde18db8067c25d20f3
Cherron Simes <cherron@developdbycherron.com>
2022-04-21 20:16:35 -0500

    Added email icon, centered layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,48 @@ import React, { Component } from 'react';
 import github from './assets/github-circle.svg';
 import linkedin from './assets/linkedin-circle.svg';
 import instagram from './assets/instagram-circle.svg';
-import logo from './assets/transparent.svg'
+import email from './assets/email-circle.svg';
+import logo from './assets/transparent.svg';
 import './App.css';
-import ContactForm from "./components/ContactForm";
+import ContactForm from './components/ContactForm';
 
 class App extends Component {
+  onEmailClick() {
+    window.open('mailto:cherron@developdbycherron.com?subject=Hi Cherron!');
+  }
+
   render() {
     return (
-    <div className='hero'>
-      <img className="logo" src={logo} alt="" />
-      <div>
-        <div className="intro">
-            <h1>Hey, I'm </h1><h1 className="name">Cherron!</h1>
-              <h1>Web Developer and owner of</h1> <h1 className="name">developd.</h1>
+      <div className='hero'>
+        <img className='logo' src={logo} alt='' />
+        <div className='intro'>
+          <h1>Hey, I'm </h1>
+          <h1 className='name'>Cherron!</h1>
+          <h1>Web Developer and owner of</h1>{' '}
+          <h1 className='name'>developd.</h1>
         </div>
-     </div>
-        <div className="socials-group">
-          <a href="https://github.com/csimes"><img className="social" src={github} alt="" /></a>
-          <a href="https://www.linkedin.com/in/cherron-simes-1b016614b"><img className="social" src={linkedin} alt="" /></a>
-          <a href="https://www.instagram.com/chocolithunda"><img className="social" src={instagram} alt="" /></a>
+        <div className='socials-group'>
+          <a href='https://github.com/csimes'>
+            <img className='social' src={github} alt='' />
+          </a>
+          <a href='https://www.linkedin.com/in/cherron-simes-1b016614b'>
+            <img className='social' src={linkedin} alt='' />
+          </a>
+          <a href='https://www.instagram.com/chocolithunda'>
+            <img className='social' src={instagram} alt='' />
+          </a>
+          <a
+            href=''
+            onClick={() =>
+              (window.location.href =
+                'mailto:cherron@developdbycherron.com?subject=Hi Cherron!')
+            }
+          >
+            <img className='social' src={email} alt='' />
+          </a>
         </div>
-         <ContactForm />
-    </div>
+        <ContactForm />
+      </div>
     );
   }
 }
--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,27 +1,16 @@
-import React from 'react';
 import { useForm, ValidationError } from '@formspree/react';
 
 function ContactForm() {
-  const [state, handleSubmit] = useForm("mrgjywaq");
+  const [state, handleSubmit] = useForm('mrgjywaq');
   if (state.succeeded) {
-      return <p>We'll let you know when the site launches!</p>;
+    return <p>We'll let you know when the site launches!</p>;
   }
   return (
-      <form onSubmit={handleSubmit}>
-      <label htmlFor="email">
-        Email Address:
-      </label>
-      <input
-        id="email"
-        type="email" 
-        name="email"
-      />
-      <ValidationError 
-        prefix="Email" 
-        field="email"
-        errors={state.errors}
-      />
-      <button type="submit" disabled={state.submitting}>
+    <form className='contact-form' onSubmit={handleSubmit}>
+      <label htmlFor='email'>Email Address:</label>
+      <input id='email' type='email' name='email' />
+      <ValidationError prefix='Email' field='email' errors={state.errors} />
+      <button type='submit' disabled={state.submitting}>
         Submit
       </button>
     </form>
